f1b5e5a9e978163528e4b5744d1669669c5e4f83
Sokomine <wegwerf@anarres.dyndns.org>
2018-08-03 17:21:06 +0200

    store which node belongs at the place of the dig_here indicator

--- place_buildings.lua
+++ place_buildings.lua
@@ -705,7 +705,7 @@ handle_schematics.generate_building = function(pos, minp, maxp, data, param2_dat
 					-- the existing node is not air, scaffolding, special scaffolding or a dig-here-indicator
 					-- -> the existing node needs to be digged
 					if(new_content and new_content ~= node_content
-					   and node_content ~= cid.c_air and node_content ~= c_scaffolding and node_content ~= c_scaffolding_empty and node_content ~= c_dig_here) then
+					   and node_content ~= cid.c_air and node_content ~= c_scaffolding and node_content ~= c_scaffolding_empty) then
 						local h;
 						-- search upward for the first empty (air) node or dig-here-indicator and place a dig_here-indicator
 						for h=ay, maxp.y do
